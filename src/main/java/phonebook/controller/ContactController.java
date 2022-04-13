0e1417045e95e45dd280e4280f8862db5483fa6e
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-13 13:22:11 +0300

    test

--- src/main/java/phonebook/controller/ContactController.java
+++ src/main/java/phonebook/controller/ContactController.java
@@ -1,18 +1,31 @@
 package phonebook.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import phonebook.entity.Contact;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class ContactController {
 
     List<Contact> contacts;
-    public ContactController(){
-        this.contacts= new ArrayList<>();
+
+    public ContactController() {
+        this.contacts = new Arrays.asList(
+                new Contact("Tedi", "+35988228884"),
+                new Contact("George"," +359882221212"),
+        )
 
     }
 
+    @GetMapping("/")
+    public ModelAndView getAllContacts(ModelAndView modelAndView) {
+        modelAndView.addObject("contatacts", contacts);
+        return modelAndView;
+    }
 }
+
